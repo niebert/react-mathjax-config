88178804d2427f3fdaf50485f3c6d201a4deeaf9
Engelbert Niehaus <niehausbert@gmail.com>
2019-04-10 15:35:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 /* @flow */
-import Provider from './Provider';
+/* CDN for MathJax is used, not offline use of mathematical expressions */
+import Provider from './Provider';  
+/* MathJax is assumed to be installed locally as subdirectory 'mathjax/' of app  */
+// import Provider from './MathJaxLocal';  
 import Node from './Node';
 
 const MathJax = {
